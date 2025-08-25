496f8e0f34054916513f5101b2fc954acf27d0fa
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2025-08-25 15:05:46 +0530

    Anyawave

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,9 +3,16 @@ import { genres } from '../assets/constants';
 const Discover = ()=>{
     console.log(genres);
 
+
+     const genreTitle = 'Pop';
     return(
-        <div>
-            Discover
+        <div className="flex flex-col">
+           <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+            <h2 className="font-bold text-3xl text-white text-left">
+                Discover {genreTitle}
+            </h2>
+                <select></select>
+           </div>
         </div>
     )
 }
